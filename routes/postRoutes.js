65bdf017814106cead2f18f8de283984d48551ac
Grindmeow <thehumaneatermeow@gmail.com>
2024-01-18 17:32:22 +0545

    changes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAllPosts } = require('../controllers/posts');
+const { getAllPosts, getPost, deletePost, updatePost, createPost } = require('../controllers/posts');
 const { fileCheck } = require('../middlewares/file_check');
 
 const notAllowed = (req, res) => res.status(405).json('method not allowed');
 
 router.route('/api/posts').get(getAllPosts).all(notAllowed);
+router.route('/api/posts/:id').get(getPost).delete(deletePost).patch(updatePost).all(notAllowed)
+router.route('/api/posts').post(fileCheck, createPost).all(notAllowed);
 
 module.exports = router;
